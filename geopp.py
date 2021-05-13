a0047dc77a6b854eb4e341ea2f953450d754c590
wutek003 <79467669+wutek003@users.noreply.github.com>
2021-05-13 13:02:41 +0200

    Update geopp.py

--- geopp.py
+++ geopp.py
@@ -58,3 +58,22 @@ plt.autoscale(True)
 ax.set_axis_on()
 plt.axis('equal');
 plt.title('Ludzie w województwach')
+
+gdf=gpd.read_file('PD_STAT_GRID_CELL_2011.shp')
+gdf=gdf.to_crs("EPSG:4326")
+gdf['centroid']=gdf.centroid
+gdw=gpd.read_file('Województwa.shp')
+gdw=gdw.to_crs("EPSG:4326")
+cell = gpd.GeoDataFrame(gdw, columns=['geometry'])
+ax=gdf.plot(markersize=.1, figsize=(12, 8), column='TOT_0_14', cmap='jet')
+plt.autoscale(False)
+cell.plot(ax=ax, facecolor="none", edgecolor='grey')
+ax.axis("off")
+merged = gpd.sjoin(gdf, cell, how='left', op='within')
+dissolve = merged.dissolve(by="index_right", aggfunc="sum")
+cell.loc[dissolve.index, 'TOT_0_14'] = dissolve.TOT_0_14.values
+ax = cell.plot(column='TOT_0_14', figsize=(12, 8), cmap='viridis', edgecolor="grey", legend = True)
+plt.autoscale(True)
+ax.set_axis_on()
+plt.axis('equal');
+plt.title('Liczba ludzi w wieku 0-14 lat w poszczególnych województwach')
